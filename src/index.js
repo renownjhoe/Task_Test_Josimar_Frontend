ac90b9a346be73472c95961383a2ffb0a0648b7d
Josimar Akpomudia <renownjosimar@gmail.com>
2025-02-26 08:18:31 +0100

    Update
    update

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-// src/index.js
 // src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot from react-dom/client
@@ -31,7 +12,6 @@ const token = localStorage.getItem('token');
 if (token) {
   store.dispatch({ type: 'auth/login', payload: { token } }); // Update with your login action
 }
-
 
 // Get the root element
 const container = document.getElementById('root');
